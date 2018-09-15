24da15f9ec36a0fe87a58b8bca57bb94055bd354
gwyrwch <gwyrwch@mail.ru>
2018-09-15 14:17:43 +0300

    Rita added  TimeModePicker

--- test132132/Views/EditorPage.xaml.cs
+++ test132132/Views/EditorPage.xaml.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
 using Xamarin.Forms;
 
 namespace test132132
 {
     public partial class EditorPage : ContentPage
     {
+
         public EditorPage()
         {
             InitializeComponent();
             SubjectPicker.ItemsSource = App.Subjects;
+            TimeModePicker.ItemsSource = Common.Mappers.AvaliableTimeModes();
         }
     }
 }
